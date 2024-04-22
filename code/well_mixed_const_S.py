578a92f726354c6d8933472401ba421c150c56f1
2BeAsh <109065992+2BeAsh@users.noreply.github.com>
2024-04-22 16:07:34 +0200

    simple model

---
+++ code/well_mixed_const_S.py
@@ -0,0 +1,122 @@
+# Well mixed
+# Fixed fractionsuscpetible
+# At each time step, pick three people. If...
+    # 2 are I, 1 is S, S -> E
+    # 1 is I, >1 is E, one of E->I
+    # Otherwise skip
+
+# Number explanation:
+    # -1 == Non-S (Cannot be converted)
+    # 0 == S (Susceptible)
+    # 1 == E (Exposed)
+    # 2 == I (Cultist)
+    
+    
+# -- Imports --
+import numpy as np
+import matplotlib.pyplot as plt
+import general_functions as gf
+from tqdm import tqdm
+plt.style.use("code/presentation.mplstyle")
+
+# -- Parameters --
+rng = np.random.default_rng()
+non_S_val = -1
+S_val = 0
+E_val = 1
+I_val = 2
+dir_path = "code/"
+dir_path_image = dir_path + "image/well_mixed_const_S_"
+dir_path_output = dir_path + "output/well_mixed_const_S_"
+
+
+def initial_population(N_tot: int, N_S: int, N_I: int):
+    population = non_S_val * np.ones(N_tot)
+    population_possible_indices = np.arange(N_tot)
+    # Get indices for S and I
+    idx_S = rng.choice(a=population_possible_indices, size=N_S, replace=False)  # Draw N_S indices from population
+    idx_I = rng.choice(a=population_possible_indices[~idx_S], size=N_I, replace=False)  # Same, but cannot draw same as S
+    # Update values
+    population[idx_S] = S_val
+    population[idx_I] = I_val
+    return population
+
+    
+def attempt_conversion(target: int, init1: int, init2: int) -> int:
+    """Given the state of three people, one target and two conversation initialisers, check if a conversion happens.
+
+    Args:
+        target (int): State of the possible convertee.
+        init1 (int): State of the first conversation initialiser.
+        init2 (int): State of the second conversation initialiser.
+
+    Returns:
+        int: target's updated value. 
+    """
+    # Target cannot be I
+    # Target is Susceptible, both initialisers are cultists. S - > E
+    if (target == S_val) and (init1 == I_val) and (init2 == I_val):
+        target = E_val
+    # Target is Exposed, at least one initialiser is cultist. E - > I
+    elif (target == E_val) and ((init1 == I_val) or (init2 == I_val)):
+        target = I_val
+    return target
+
+
+def evolve(N_tot, N_S, N_I, time_steps, dir_path_file=dir_path_output):    
+    population = initial_population(N_tot, N_S, N_I)
+    population_history = np.empty((N_tot, time_steps))
+    population_history[:, 0] = 1 * population
+    for i in tqdm(range(1, time_steps)):
+        # Pick three people, one target and two conversation initialisers. 
+        # Then get their states
+        people_picked_idx = rng.choice(np.arange(N_tot), size=3, replace=False)
+        target_idx, init1_idx, init2_idx = people_picked_idx
+        target_state = population[target_idx]
+        init1_state = population[init1_idx]
+        init2_state = population[init2_idx]
+        # Perform conversion attempt and update target's state
+        target_new_state = attempt_conversion(target_state, init1_state, init2_state)
+        population[target_idx] = target_new_state
+        # Store current population in history
+        population_history[:, i] = 1 * population  # Need a copy
+    
+    # Write to file
+    np.savetxt(fname=dir_path_file + "population_history.gz", X=population_history)
+    
+
+def plot_SIE():
+    # Load data
+    population_history = np.genfromtxt(dir_path_output+"population_history.gz")
+    N_tot, time = np.shape(population_history)
+    S_history = np.count_nonzero(population_history==S_val, axis=0)
+    E_history = np.count_nonzero(population_history==E_val, axis=0)
+    I_history = np.count_nonzero(population_history==I_val, axis=0)
+    
+    # Normalize
+    S_history = S_history / N_tot
+    E_history = E_history / N_tot
+    I_history = I_history / N_tot
+    # Plot
+    fig, ax = plt.subplots()
+    time_vals = np.arange(time)
+    ax.plot(time_vals, S_history, "-", label="Susceptible")
+    ax.plot(time_vals, E_history, "--", label="Exposed")
+    ax.plot(time_vals, I_history, "-.", label="Cultist")
+    ax.legend(bbox_to_anchor=(0.5, 1.05), ncol=3, loc="lower center")
+    figname = dir_path_image + "SIE_plot.png"
+    plt.savefig(figname)
+    plt.show()
+    
+    
+if __name__ == "__main__":
+    N_tot = 1000
+    frac_S = 0.8
+    frac_I = 0.1
+    time_steps = 35_000
+    
+    get_data = True
+    if get_data:
+        evolve(N_tot, int(N_tot*frac_S), int(N_tot*frac_I), time_steps)
+    
+    plot_SIE()
