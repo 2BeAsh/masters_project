5596e03bcded7bfba389583f0be9336552e498cd
2BeAsh <109065992+2BeAsh@users.noreply.github.com>
2024-07-29 08:54:40 +0200

    neighbour width

--- code/debt_deflation_1d.py
+++ code/debt_deflation_1d.py
@@ -4,7 +4,9 @@ from debt_deflation_well_mixed import DebtDeflation, N_agents, time_steps, inter
 
 
 class DebtDeflation_1d(DebtDeflation):
-    def __init__(self, number_of_companies: int, money_to_production_efficiency: float, interest_rate: float, buy_fraction: float, equilibrium_distance_fraction: float, include_debt: bool, time_steps: int):
+    def __init__(self, number_of_companies: int, money_to_production_efficiency: float, 
+                 interest_rate: float, buy_fraction: float, equilibrium_distance_fraction: float, 
+                 neighbour_width: int, include_debt: bool, time_steps: int):
         """Initializer
 
         Args:
@@ -16,29 +18,53 @@ class DebtDeflation_1d(DebtDeflation):
             equilibrium_distance_fraction (float): _description_
             time_steps (int): _description_
         """
+        # Lots of overlap with well mixed
         super().__init__(number_of_companies, money_to_production_efficiency, interest_rate, buy_fraction, equilibrium_distance_fraction, include_debt, time_steps)
-        self.file_parameter_addon = f"Steps{self.time_steps}_Companies{self.N}_Interest{self.r}_Efficiency{self.alpha}_BuyFraction{self.buy_fraction}_EquilibriumStep{self.epsilon}_1D"
+        
+        # New parameters
+        self.nbor = neighbour_width
+        
+        # Updated image path
+        self.dir_path_image = self.dir_path + "image/" + "1d/"
+        self.file_parameter_addon = f"Steps{self.time_steps}_Companies{self.N}_Interest{self.r}_Efficiency{self.alpha}_BuyFraction{self.buy_fraction}_EquilibriumStep{self.epsilon}_nborwidth{self.nbor}_1D"
     
     
-    def _buyer_seller_idx_1d(self) -> tuple:
+    def _seller_idx(self, buyer_idx) -> int:
+        nbor_dist = np.random.randint(-self.nbor, self.nbor + 1)
+        seller_idx = buyer_idx + nbor_dist
+        if seller_idx >= self.N:  # Right boundary
+            seller_idx = buyer_idx - 1
+        elif seller_idx < 0:  # Left boundary
+            seller_idx = buyer_idx + 1
+
+        return seller_idx
+    
+    
+    def _buyer_seller_idx_money_scaling_1d(self) -> tuple:
         """Pick buyer based proportional to money, pick seller as one of its neighbours
         """
         # Buyer is prop to money
         money_wrt_negative_vals = np.maximum(self.money, 0)  # If has negative money, give a 0 chance to buy.
         buyer_prob = money_wrt_negative_vals / money_wrt_negative_vals.sum()  # The probability to buy is proportional to one's money. 
         buyer_idx = np.random.choice(np.arange(self.N), p=buyer_prob)
-        
-        # Seller one of its neighbours, taking closed boundaries into account
-        minus_or_plus_one = 2 * np.random.randint(low=0, high=2) - 1
-        seller_idx = buyer_idx + minus_or_plus_one
-        if seller_idx == self.N:  # Right boundary
-            seller_idx = buyer_idx - 1
-        elif seller_idx < 0:  # Left boundary
-            seller_idx = buyer_idx + 1
+        # Seller
+        seller_idx = self._seller_idx(buyer_idx)
         
         return buyer_idx, seller_idx
         
+
+    def _buyer_seller_idx_uniform_1d(self) -> tuple:
+        """Pick buyer randomly, pick seller as one of its neighbours
+        """
+        # Buyer is prop to money
+        buyer_idx = np.random.randint(low=0, high=self.N)
         
+        # Seller
+        seller_idx = self._seller_idx(buyer_idx)
+        
+        return buyer_idx, seller_idx
+
+
     def simulation(self) -> None:
         # Initialize market
         self._initial_market()
@@ -55,13 +81,14 @@ class DebtDeflation_1d(DebtDeflation):
             # Make N transactions
             for _ in range(self.N):
                 # Pick buyer and seller
-                buyer_idx, seller_idx = self._buyer_seller_idx_1d()
+                # buyer_idx, seller_idx = self._buyer_seller_idx_money_scaling_1d()
+                buyer_idx, seller_idx = self._buyer_seller_idx_uniform_1d()
                 self._step(buyer_idx, seller_idx)
             # Pay rent and check for bankruptcy
             if self.include_debt:
                 self._pay_rent()
                 self._bankruptcy_check()
-            self._inflation(self.epsilon)
+            self._inflation()
             # Store values
             self.production_hist[:, i] = self.production
             self.debt_hist[:, i] = self.debt
@@ -71,16 +98,24 @@ class DebtDeflation_1d(DebtDeflation):
         self._data_to_file()
 
 
+# Parameters unique to 1d i.e. not used in well mixed
+neighbour_width = 2
 
+
+# Create instance of class 
 debtdeflation_1d = DebtDeflation_1d(number_of_companies=N_agents, 
                             money_to_production_efficiency=money_to_production_efficiency, 
                             interest_rate=interest, 
                             buy_fraction=buy_fraction, 
                             equilibrium_distance_fraction=equilibrium_distance_fraction, 
+                            neighbour_width=neighbour_width,
                             time_steps=time_steps,
                             include_debt=True)
+
+# Get filename path for plotting
 filename_parameter_addon_1d = debtdeflation_1d.file_parameter_addon
 
 
 if __name__ == "__main__":
+    # Run the simulation
     debtdeflation_1d.simulation()
