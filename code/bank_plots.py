b0081c5f35127d9a128dcc9990ec2e88fc839edc
2BeAsh <109065992+2BeAsh@users.noreply.github.com>
2024-09-16 13:31:59 +0200

    Deriv plots

--- code/bank_plots.py
+++ code/bank_plots.py
@@ -32,6 +32,8 @@ class BankVisualization(DebtDeflationVisualization):
         self.interest_rate = data_all[0, :, 4]  # Identical for all N companies on the 0-axis, so choose one
         self.beta = data_all[:, :, 5]  
         self.did_not_take_loan = data_all[0, :, 6]  # Identical for all N companies on the 0-axis, so choose one
+        self.first_derivative = data_all[0, :, 7]
+        self.second_derivative = data_all[1, :, 7]
         
         # Derivates of data
         self.bank_money = self.bank_fortune - np.sum(self.debt, axis=0)
@@ -39,7 +41,34 @@ class BankVisualization(DebtDeflationVisualization):
         self.time_vals = np.arange(self.time_steps)
         self.N = np.shape(self.production)[0]
         
-    
+
+    def _count_consecutive_signs(self, arr) -> list:
+        """Count consecutive positive/negative values in an array. Negative values are recorded as minus their count
+
+        Args:
+            arr (ndarray): Array of values 
+
+        Returns:
+            list: Counts of consecutive positive/negative values
+        """
+        result = []
+        count = 1
+        sign = np.sign(arr[0])
+        
+        for i in range(1, len(arr)):
+            current_sign = np.sign(arr[i])
+            if current_sign == sign:  # Consecutive of same values increase count
+                count += 1
+            else:  # Change of sign, record current count and reset
+                result.append(count if sign > 0 else -count) 
+                count = 1
+                sign = current_sign
+        
+        # Append last count
+        result.append(count if sign > 0 else -count)
+        return result
+
+        
     def plot_bank_fortune(self):
         fig, ax = plt.subplots()
         
@@ -57,12 +86,12 @@ class BankVisualization(DebtDeflationVisualization):
         ax.plot(self.time_vals, debt_summed, label="Debt", marker=".", markersize=2)
         
         # Bank fortune 
-        ax.plot(self.time_vals, self.bank_fortune, "--", color="black", label="Abs Bank fortune", markersize=1, alpha=0.7)
+        ax.plot(self.time_vals, self.bank_fortune, "--", color="black", label="Bank fortune", alpha=0.9)
         
         # Axis setup
         ax.set(ylabel="Log \$", title="Bank fortune and its components", yscale="log")
         ax.grid()
-        ax.legend()
+        ax.legend(ncols=4, bbox_to_anchor=(0.5, 0.95), loc="lower center", fontsize=6)
         
         self._add_parameters_text(ax)
         self._save_fig(fig, name="bankfortune")
@@ -167,9 +196,111 @@ class BankVisualization(DebtDeflationVisualization):
         fig, ax = plt.subplots()
         ax.plot(self.time_vals[1:], fraction_did_not_take_loan)        
         ax.set(xlabel="Time", ylabel=r"$n/N$", title=f"Fraction of companies that did not take a loan, N={self.N}")
-        
+        # Add parameters
+        self._add_parameters_text(ax)
+        # Save and show fig
+        self._save_fig(fig, name="no_loan")
         if show_plots: plt.show()
+            
+    
+    def plot_derivatives(self):
+        """First column is log scale of first and second derivative, with abs(negative values) in red and positive in green.
+        Second column show only the sign of the first and second derivative.
+        """
+        # Create figure and extract axis
+        fig, ax_arr = plt.subplots(nrows=2, ncols=2, figsize=(16, 8))
+        ax = ax_arr[0, 0]
+        ax1 = ax_arr[0, 1]
+        ax2 = ax_arr[1, 0]
+        ax3 = ax_arr[1, 1]
+                
+        # Create masks for showing positive and negative values on log scale
+        mask_neg_1st = self.first_derivative <= 0
+        mask_pos_1st = self.first_derivative > 0
+        mask_neg_2nd = self.second_derivative <= 0
+        mask_pos_2nd = self.second_derivative > 0
+        time_neg_1st = self.time_vals[mask_neg_1st]
+        time_pos_1st = self.time_vals[mask_pos_1st]
+        time_neg_2nd = self.time_vals[mask_neg_2nd]
+        time_pos_2nd = self.time_vals[mask_pos_2nd]
         
+        # Change any values of zero to a small value to avoid log(0) = -inf
+        self.first_derivative[self.first_derivative == 0] = 1e-3
+        self.second_derivative[self.second_derivative == 0] = 1e-3
+
+        # Find where the sign changes
+        # Identify sign change points for first and second derivatives
+        sign_change_1st = np.diff(np.sign(self.first_derivative)) != 0
+        sign_change_2nd = np.diff(np.sign(self.second_derivative)) != 0
+
+        # Time values where sign changes occur
+        time_change_1st = self.time_vals[:-1][sign_change_1st]  # :-1 because difference is one less than original
+        time_change_2nd = self.time_vals[:-1][sign_change_2nd]
+
+        # First column - Log scale values
+        ax.plot(time_neg_1st, np.abs(self.first_derivative[mask_neg_1st]), ".", color="red", label="Negative", markersize=1)
+        ax.plot(time_pos_1st, self.first_derivative[mask_pos_1st], ".", color="green", label="Positive", markersize=1) 
+        ax1.plot(time_neg_2nd, np.abs(self.second_derivative[mask_neg_2nd]), ".", color="red", markersize=1, label="Negative")
+        ax1.plot(time_pos_2nd, self.second_derivative[mask_pos_2nd], ".", color="green", markersize=1, label="Positive")
+        
+        # Second column - Sign values at +1 and -1
+        ax2.plot(time_neg_1st, -np.ones_like(time_neg_1st), ".", color="red", markersize=2, label="Negative")
+        ax2.plot(time_pos_1st, np.ones_like(time_pos_1st), ".", color="green", markersize=2, label="Positive")
+        ax3.plot(time_neg_2nd, -np.ones_like(time_neg_2nd), ".", color="red", markersize=2, label="Negative") 
+        ax3.plot(time_pos_2nd, np.ones_like(time_pos_2nd), ".", color="green", markersize=2, label="Positive")
+        
+        # Add markers at sign change points
+        ax2.plot(time_change_1st, np.zeros_like(time_change_1st), ".", color="blue", markersize=1, label="Sign change")
+        ax3.plot(time_change_2nd, np.zeros_like(time_change_2nd), ".", color="blue", markersize=1, label="Sign change")
+        
+        # Axis setup
+        ax.set(ylabel="Log Derivative", title="First derivative", yscale="log")
+        ax1.set(xlabel="Time", title="Second derivative", yscale="log")
+        ax2.set(ylabel="Sign of derivative", title="Sign of first derivative", yticks=[-1, 1], ylim=(-1.5, 1.5), xlabel="Time")
+        ax3.set(title="Sign of second derivative", yticks=[-1, 1], ylim=(-1.5, 1.5), xlabel="Time")
+        
+        # Legend
+        ax.legend(ncols=2, bbox_to_anchor=(0.5, 0.8), loc="lower center")
+        ax1.legend(ncols=2, bbox_to_anchor=(0.5, 0.8), loc="lower center")
+        ax2.legend(ncols=3, bbox_to_anchor=(0.5, 0.9), loc="lower center")
+        ax3.legend(ncols=3, bbox_to_anchor=(0.5, 0.9), loc="lower center")
+        
+        # Grid
+        ax.grid()
+        ax1.grid() 
+        ax2.grid() 
+        ax3.grid()
+        # Add parameters
+        self._add_parameters_text(ax, x=0.05)
+        # Save and show fig
+        self._save_fig(fig, name="derivatives")
+        if self.show_plots: plt.show()
+
+
+    def plot_consecutive_counts(self):
+        """Plot consecutive counts of positive and negative values for first and second derivatives."""
+        # Calculate consecutive counts
+        consecutive_counts_1st = self._count_consecutive_signs(self.first_derivative)
+        consecutive_counts_2nd = self._count_consecutive_signs(self.second_derivative)
+        
+        # Create figure and extract axis
+        fig, (ax, ax1) = plt.subplots(nrows=2)
+        
+        # Plot consecutive counts for first derivative
+        ax.plot(consecutive_counts_1st, "-", markersize=2)        
+        ax1.plot(consecutive_counts_2nd, "-", markersize=2)
+
+        ax.set(ylabel="Counts", title="First Derivative")
+        ax1.set(xlabel="Index", ylabel="Counts", title="Second Derivative")
+
+        # Grid
+        ax.grid()
+        ax1.grid()
+        
+        # Save and show plot
+        self._save_fig(fig, name="consecutive_counts")
+        if show_plots: plt.show()
+
 
 if __name__ == "__main__": 
     run_wm = True  # Well mixed
@@ -181,13 +312,15 @@ if __name__ == "__main__":
         print("Plotting Well Mixed")
         visualize = BankVisualization(filename_parameter_addon, show_plots)
         
-        visualize.plot_companies(N_plot=4, scale=scale)
+        # visualize.plot_companies(N_plot=4, scale=scale)
         visualize.plot_means(scale)
         visualize.final_time_values(scale)
         visualize.final_time_size_dist()
         visualize.plot_bank_fortune()
         visualize.plot_beta_evolution()
         visualize.plot_did_not_take_loan()
+        visualize.plot_derivatives()
+        visualize.plot_consecutive_counts()
         
         if animate:
             visualize.animate_values(scale)
