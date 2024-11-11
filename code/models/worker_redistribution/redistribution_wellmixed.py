d0eaabcded44c9742c59592f5e84174f4966e4d5
2BeAsh <109065992+2BeAsh@users.noreply.github.com>
2024-11-11 11:52:21 +0100

    machine cost as variable

--- code/models/worker_redistribution/redistribution_wellmixed.py
+++ code/models/worker_redistribution/redistribution_wellmixed.py
@@ -1,11 +1,11 @@
 import numpy as np
 from tqdm import tqdm
 from pathlib import Path
-from redistribution_master import Workforce, number_of_companies, number_of_workers, salary_increase, time_steps
+from redistribution_master import Workforce, number_of_companies, number_of_workers, salary_increase, machine_cost, time_steps
 
 
 class BankWellMixed(Workforce):
-    def __init__(self, number_of_companies, number_of_workers, salary_factor, time_steps):
+    def __init__(self, number_of_companies, number_of_workers, salary_factor, machine_cost, time_steps):
         """_summary_
 
         Args:
@@ -14,15 +14,20 @@ class BankWellMixed(Workforce):
             time_steps (int): _description_
         """
         # Get master methods
-        super().__init__(number_of_companies, number_of_workers, salary_factor, time_steps)
+        super().__init__(number_of_companies, number_of_workers, salary_factor, machine_cost, time_steps)
 
     
     def run_store_values(self):
         self.store_values()
         
         
-bank_wellmixed = BankWellMixed(number_of_companies, number_of_workers, salary_increase, time_steps)
+    def run_store_peak_rho_space(self):
+        self.store_peak_rho_space()
+        
+        
+bank_wellmixed = BankWellMixed(number_of_companies, number_of_workers, salary_increase, machine_cost, time_steps)
 
 if __name__ == "__main__":
-    bank_wellmixed.run_store_values()
+    # bank_wellmixed.run_store_values()
+    bank_wellmixed.run_store_peak_rho_space()
     print("Stored Values")
